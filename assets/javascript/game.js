717fac74ae7034d8332a95c8475d8f8f121f452a
Artur Markov <artur.markov1860@gmail.com>
2019-07-03 22:13:45 -0700

    alto and cello work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,128 +1,171 @@
-var options = ["violin"]//, "cello", "alto", "trumpet", "guitar", "piano", "saxophone", "clarinet", "flute"];
+var options = ["violin", "cello", "alto", "trumpet", "guitar", "piano", "saxophone", "clarinet", "flute"];
+
 
 var points = 0; //wins
 var guess = 12;
 var word = document.getElementById("word");
+var par = document.getElementById("par");                          //var for Letters Already Guessed paragraph
 
-var varIndex = Math.floor(Math.random() * options.length);         //random word
-var guessWord = options[varIndex];                                 //Computer's random chosen word from "options" array
-console.log(guessWord);
-
-var par = document.getElementById("par");                            //var for Letters Already Guessed paragraph
-
-
-if (guessWord === "violin") {                                        //if word "violin" is chousen
-
-    var wordToGuess = guessWord.split("");                           //we want to split our guessWord on elements because we can't 
-                                                                     // change every character, bua we can change every separate string 
-    document.onkeypress = keyPressed;
-
-    var wordToDisplay = Array(guessWord.length).fill("_");           //in start of game we want't to display guessWord like "_ _ _ _ _ _"
-
-    word.textContent = wordToDisplay.join(" ");                      //then we take our split word back to the whole word
-
-
-    function keyPressed() {                                          //function for game 
-
-        console.log(wordToGuess)
-        if (word.textContent.includes(event.key)){                   //put restrictions for doubled correct letters
-            word.textContent = word.textContent;
-            console.log("you typed this letter");
-        } else if (event.key == "i") {                               // word "violin" has two "i", so, we want to chahge both
-            wordToDisplay[1] = event.key;
-            wordToDisplay[4] = event.key;
-            word.textContent = wordToDisplay.join(" ");
-        } else if (par.textContent.includes(event.key)) {            //put restrictions for doubled incorrect letters 
-            par.textContent = par.textContent;
-            console.log("guessed letter")
-        } else if (!(wordToGuess.includes(event.key))) {             //if you tiped incorrect letter, points goes down
-
-            if (guess == 1) {                                        //if guess == 0; then refresh the page
-                alert("you lose!");
-                document.location.reload();
-            }
-            guess = guess - 1;
-            document.querySelector(".guess").innerText = guess;
-            par.innerText = par.textContent + (event.key) + ", ";
-            console.log("-1 point");
-        }  else { 
-            wordToDisplay[wordToGuess.indexOf(event.key)] = event.key;
-            word.textContent = wordToDisplay.join(" ");
-            console.log(event.key);
-        } 
-
-    }
-        function win() {
-        if (wordToDisplay == wordToGuess) {
-            points = points + 1;
-            document.querySelector(".wins").innerText = points;
-            console.log("you win!")
-        }
+function lose() {                                                  //if player got 0 tries, then refresh the page
+    if (guess == 0) {
+        alert("you lose!");
+        document.location.reload();
     }
 }
 
+function takeWord() {
+        var varIndex = Math.floor(Math.random() * options.length);         //random word
+        var guessWord = options[varIndex];                                 //Computer's random chosen word from "options" array
+        console.log(guessWord);
+
+        if (guessWord === "violin") {                                        //if word "violin" is chousen
+
+            var wordToGuess = guessWord.split("");                           //we want to split our guessWord on elements because we can't 
+                                                                            // change every character, bua we can change every separate string 
+            document.onkeypress = keyPressed;
+
+            var wordToDisplay = Array(guessWord.length).fill("_");           //in start of game we want't to display guessWord like "_ _ _ _ _ _"
+
+            word.textContent = wordToDisplay.join(" ");
+
+            function win() {                                                 //function for winning
+                if (word.textContent == "v i o l i n") {
+                            console.log("you win!");
+                            points = points + 1;
+                            document.querySelector(".wins").innerText = points;
+                            takeWord();
+                        } 
+                    }
+            
 
 
+            function keyPressed() {                                          //function for game 
 
-    // QUESTION 3. function keyPressed doesn't work outside of loop
+                console.log(wordToGuess)
+                if (word.textContent.includes(event.key)){                   //put restrictions for doubled correct letters
+                    word.textContent = word.textContent;
+                    console.log("you typed this letter");
+                } else if (event.key == "i") {                               // word "violin" has two "i", so, we want to chahge both
+                    wordToDisplay[1] = event.key;
+                    wordToDisplay[4] = event.key;
+                    word.textContent = wordToDisplay.join(" ");
+                    win();
+                } else if (par.textContent.includes(event.key)) {            //put restrictions for doubled incorrect letters 
+                    par.textContent = par.textContent;
+                    console.log("guessed letter")
+                } else if (!(wordToGuess.includes(event.key))) {             //if you tiped incorrect letter, points goes down
+                    guess = guess - 1;
+                    document.querySelector(".guess").innerText = guess;
+                    par.innerText = par.textContent + (event.key) + ", ";
+                    console.log("-1 point");
+                    lose();
+                }  else { 
+                    wordToDisplay[wordToGuess.indexOf(event.key)] = event.key;
+                    word.textContent = wordToDisplay.join(" ");
+                    console.log(event.key);
+                    win();
+                } 
+            }
+        }   else if (guessWord === "cello") {
+
+            var wordToGuess = guessWord.split("");                           //we want to split our guessWord on elements because we can't 
+                                                                            // change every character, bua we can change every separate string 
+            document.onkeypress = keyPressed;
+
+            var wordToDisplay = Array(guessWord.length).fill("_");           //in start of game we want't to display guessWord like "_ _ _ _ _ _"
+
+            word.textContent = wordToDisplay.join(" ");
+
+            function win() {                                                 //function for winning
+                if (word.textContent == "c e l l o") {
+                            console.log("you win!");
+                            points = points + 1;
+                            document.querySelector(".wins").innerText = points;
+                            takeWord();
+                        } 
+                    }
+            
 
 
+            function keyPressed() {                                          //function for game 
 
-    // function keyPressed() {                                         //function for game with word "violin"
-    //                   if (event.key == guessWord[guessWord.length - 1]) {
-    //                     points = points + 1;
-    //                     document.querySelector(".wins").innerText = points;
-    //                     document.querySelector("#word").innerText = points;
-    //                     word[5].innerText = "n";
-    //                 } else if  (event.key == guessWord[guessWord.length - 2]) {
-    //                     points = points + 1;
-    //                     document.querySelector(".wins").innerText = points;
-    //                 } else if  (event.key == guessWord[guessWord.length - 3]) {
-    //                     points = points + 1;
-    //                     document.querySelector(".wins").innerText = points;
-    //                 } else if  (event.key == guessWord[guessWord.length - 4]) {
-    //                     points = points + 1;
-    //                     document.querySelector(".wins").innerText = points;
-    //                 } else if  (event.key == guessWord[guessWord.length - 6]) {
-    //                     points = points + 1;
-    //                     document.querySelector(".wins").innerText = points;
-    //                 } else {
-    //                     guess = guess - 1;
-    //                     document.querySelector(".guess").innerText = guess;
-    //                     par.innerText = par.textContent + (event.key) + ", ";
-    //                             if (guess === 0) {
-    //                                 document.querySelector(".guess").innerText = "You Lose!";
-    //                                 alert("You Lose!");
-    //                                 // QUESTION 2. if i type "alert" or "confirm", it works BEFORE "You Lose"
-    //                             } 
-    //                 }
-    //     }
-    // QUESTION 4. How to stop work this loop after loosing? I put break, but it doesn't work.
-    // QUESTION 5. I should change "Wins" when whole word is typed, not every letter
+                console.log(wordToGuess)
+                if (word.textContent.includes(event.key)){                   //put restrictions for doubled correct letters
+                    word.textContent = word.textContent;
+                    console.log("you typed this letter");
+                } else if (event.key == "l") {                               // word "violin" has two "i", so, we want to chahge both
+                    wordToDisplay[2] = event.key;
+                    wordToDisplay[3] = event.key;
+                    word.textContent = wordToDisplay.join(" ");
+                    win();
+                } else if (par.textContent.includes(event.key)) {            //put restrictions for doubled incorrect letters 
+                    par.textContent = par.textContent;
+                    console.log("guessed letter")
+                } else if (!(wordToGuess.includes(event.key))) {             //if you tiped incorrect letter, points goes down
+                    guess = guess - 1;
+                    document.querySelector(".guess").innerText = guess;
+                    par.innerText = par.textContent + (event.key) + ", ";
+                    console.log("-1 point");
+                    lose();
+                }  else { 
+                    wordToDisplay[wordToGuess.indexOf(event.key)] = event.key;
+                    word.textContent = wordToDisplay.join(" ");
+                    console.log(event.key);
+                    win();
+                } 
+            }
 
-    // document.onkeyup = keyPressed;
 
-// } else if (guessWord == "cello") {
-//     var violinLets = ["c", "e","l", "o"]
-//         function keyPressed() {
-//                     if (event.key == guessWord[guessWord.length - 1]) {
-//                         points = points + 1;
-//                         document.querySelector(".wins").innerText = points;
-//                     } else if  (event.key == guessWord[guessWord.length - 2]) {
-//                         points = points + 1;
-//                         document.querySelector(".wins").innerText = points;
-//                     } else if  (event.key == guessWord[guessWord.length - 4]) {
-//                         points = points + 1;
-//                         document.querySelector(".wins").innerText = points;
-//                     } else if  (event.key == guessWord[guessWord.length - 5]) {
-//                         points = points + 1;
-//                         document.querySelector(".wins").innerText = points;
-//                     } else {
-//                         guess = guess - 1;
-//                         document.querySelector(".guess").innerText = guess;
-//                     }  
-//         }
-//                     document.onkeyup = keyPressed;
+         } else if (guessWord === "alto") {
+            var wordToGuess = guessWord.split("");                           //we want to split our guessWord on elements because we can't 
+                                                                            // change every character, bua we can change every separate string 
+            document.onkeypress = keyPressed;
+
+            var wordToDisplay = Array(guessWord.length).fill("_");           //in start of game we want't to display guessWord like "_ _ _ _ _ _"
+
+            word.textContent = wordToDisplay.join(" ");
+
+            function win() {                                                 //function for winning
+                if (word.textContent == "a l t o") {
+                            console.log("you win!");
+                            points = points + 1;
+                            document.querySelector(".wins").innerText = points;
+                            takeWord();
+                        } 
+                    }
+            
+
+
+            function keyPressed() {                                          //function for game 
+
+                console.log(wordToGuess)
+                if (word.textContent.includes(event.key)){                   //put restrictions for doubled correct letters
+                    word.textContent = word.textContent;
+                    console.log("you typed this letter");
+                } else if (par.textContent.includes(event.key)) {            //put restrictions for doubled incorrect letters 
+                    par.textContent = par.textContent;
+                    console.log("guessed letter")
+                } else if (!(wordToGuess.includes(event.key))) {             //if you tiped incorrect letter, points goes down
+                    guess = guess - 1;
+                    document.querySelector(".guess").innerText = guess;
+                    par.innerText = par.textContent + (event.key) + ", ";
+                    console.log("-1 point");
+                    lose();
+                }  else { 
+                    wordToDisplay[wordToGuess.indexOf(event.key)] = event.key;
+                    word.textContent = wordToDisplay.join(" ");
+                    console.log(event.key);
+                    win();
+                } 
+            }
+            
+         }
+
+
+}
+
+takeWord();
+
 
 // } else if (guessWord == "alto") {
 //     var violinLets = ["a", "l", "o", "t"]
